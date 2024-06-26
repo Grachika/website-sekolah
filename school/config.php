6cbb5c82e12a87ab7af2381b44a0f57a03260fe1
Grachika <grachikait@gmail.com>
2024-06-26 14:12:49 +0700

    Add files via upload

---
+++ school/config.php
@@ -0,0 +1,15 @@
+<?php
+
+//buat koneksi
+$koneksi = mysqli_connect('localhost', 'root', '', 'db_school');
+
+//cek koneksi
+// if (mysqli_connect_error()) {
+//    echo "Gagal Koneksi ke Database";
+// } else {
+//  echo "Berhasil Koneksi";
+//} 
+
+// url induk
+$main_url = "http://localhost/school/";
+?>
